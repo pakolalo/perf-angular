f696ec5c4bdeae5264bac1ce0aa5bee5b07742e5
Francisco del Collado <fedelcollado@gmail.com>
2024-01-31 16:30:03 -0600

    angular con dependencias optimizadas

--- src/app/components/customers/customers.component.ts
+++ src/app/components/customers/customers.component.ts
@@ -2,7 +2,6 @@ import { Component, ViewChild } from '@angular/core';
 import { faAngleLeft, faAngleRight } from '@fortawesome/free-solid-svg-icons';
 import { SwiperOptions } from 'swiper';
 import { SwiperComponent } from 'swiper/angular';
-import * as moment from 'moment';
 
 interface Customer {
   title: string;
@@ -56,7 +55,4 @@ export class CustomersComponent {
     this.swiper?.swiperRef?.slidePrev();
   }
 
-  calcTimeAgo(date: Date) {
-    return moment(date).fromNow();
-  }
 }
---
+++ src/app/pipes/time-ago.pipe.ts
@@ -0,0 +1,16 @@
+import { Pipe, PipeTransform } from '@angular/core';
+import { formatDistanceToNow} from 'date-fns';
+
+@Pipe({
+  name: 'timeAgo'
+})
+export class TimeAgoPipe implements PipeTransform {
+
+  transform(value: Date): string {
+    if(value) {
+      return formatDistanceToNow(value);
+    }
+    return '';
+  }
+
+}
